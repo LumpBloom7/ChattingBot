e0390083ebc6b95ca2ae31b6bf0d2f65b7635ac4
Teammie <43757589+teammiez@users.noreply.github.com>
2019-10-15 15:55:39 +0700

    Add files via upload

---
+++ chattingbotC++/cmake-build-debug/CMakeFiles/chattingbotC__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chattingbotC__.dir/src/main.cpp.obj"
+  "chattingbotC__.exe"
+  "chattingbotC__.exe.manifest"
+  "chattingbotC__.lib"
+  "chattingbotC__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chattingbotC__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
